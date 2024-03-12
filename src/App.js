d2d9dc14da59b4918c45648527dd7d8d07689e93
Vignesh <joshi.vignesh@gmail.com>
2024-03-12 15:47:43 +0530

    TASKMATE COMMIT

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 
 function App() {
+
+  function handleAdd(){
+    count+=1
+  }
+
+  let count = 0;
   return (
-    <>
-     <h1> Hi, Hello</h1>
-     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
-    </>
+    <div className='App'>
+        <div className="box">
+          <p> {count}</p>
+          <button className='add' onClick={handleAdd}>ADD</button>
+          <button className='sub' onClick={handleAdd}>SUB</button>
+        </div>
+      </div>
   );
 }
 
