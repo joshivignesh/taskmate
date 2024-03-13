0ed7416f2c3892b806566f192d5b5f909b5babfc
Vignesh <joshi.vignesh@gmail.com>
2024-03-13 14:55:12 +0530

    Added State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [count, setCount] = useState(0);  
+console.log(useState(0))
 
   function handleAdd(){
-    count+=1
+    setCount(count+1);
   }
-
-  let count = 0;
+  function handleSub(){
+    setCount(count-1);
+  }
+  function handleReset(){
+    setCount(0);
+  }
+ 
   return (
     <div className='App'>
         <div className="box">
           <p> {count}</p>
-          <button className='add' onClick={handleAdd}>ADD</button>
-          <button className='sub' onClick={handleAdd}>SUB</button>
+          <button className='add' onClick={()=> setCount(count+1) }>ADD</button>
+          <button className='sub' onClick={handleSub}>SUB</button>
+          <button className='reset' onClick={handleReset}>RESET</button>
         </div>
       </div>
   );
