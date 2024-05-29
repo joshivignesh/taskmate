d5eddd3c17b139e0c329cfd28af63a719f2a1e11
Vignesh <joshi.vignesh@gmail.com>
2024-05-29 22:44:38 +0530

    Added Header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,40 @@
 import logo from "../assets/logo.svg";
 
-const Header = ({children, theme, setTheme}) => {
-    return (
-        <header>
-            <span className="logo">
-                <img src={logo} alt="" />
-                <span>{children}</span>
-            </span>
-            <div className="themeSelector">
-                <span className="light"></span>
-                <span className="medium"></span>
-                <span className="dark"></span>
-                <span className="gOne"></span>
-                <span className="gTwo"></span>
-                <span className="gThree"></span>
-            </div>
-        </header>
-    )
-}
+const Header = ({ children, theme, setTheme }) => {
+  return (
+    <header>
+      <span className="logo">
+        <img src={logo} alt="" />
+        <span>{children}</span>
+      </span>
+      <span className="themeSelector">
+        <span
+          onClick={() => setTheme("light")}
+          className={theme === "light" ? "light activeTheme" : "light"}
+        ></span>
+        <span
+          onClick={() => setTheme("medium")}
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+        ></span>
+        <span
+          onClick={() => setTheme("dark")}
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+        ></span>
+        <span
+          onClick={() => setTheme("gOne")}
+          className={theme === "gOne" ? "gOne activeTheme" : "gOne"}
+        ></span>
+        <span
+          onClick={() => setTheme("gTwo")}
+          className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}
+        ></span>
+        <span
+          onClick={() => setTheme("gThree")}
+          className={theme === "gThree" ? "gThree activeTheme" : "gThree"}
+        ></span>
+      </span>
+    </header>
+  );
+};
 
 export default Header;
